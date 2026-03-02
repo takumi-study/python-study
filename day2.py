f61408c4bb7f8b6a7be2a661424043e5845c7533
Takumi <jobtaku.25@gmail.com>
2026-03-02 20:46:42 +0900

    Add variables and type checking practice

--- day2.py
+++ day2.py
@@ -1,1 +1,16 @@
 print("VSCode成功！")
+
+name = "Takumi"
+age = 26
+height = 178.5
+is_student = False
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
