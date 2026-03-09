6d49a2984bfade91c571721aec5499f489b6fdc8
Takumi <jobtaku.25@gmail.com>
2026-03-09 23:35:09 +0900

    Add exercises for variables, operators, and data types

---
+++ day9.py
@@ -0,0 +1,46 @@
+a = 1 + 2
+a
+print(a)
+
+a = 1
+print(a)
+a = a + 1
+print(a)
+
+kakaku = 1000
+nebikiritu = 15
+Kakaku = kakaku *(100 - nebikiritu)/100
+print(Kakaku)
+
+int(373.555)
+
+a = int(373.555)
+print(a)
+
+a = 1
+b =1/2
+c = "ABC"
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+a = 1
+b = 2
+c, d = a*2,b*2
+print(c, d)
+a, b = b, a
+print(c, d)
+
+a = 1
+b = 2
+a, b =b, a
+print(a, b)
+
+a = 1
+b = 2
+a, b =b, a
+c, d = a*2, b*2
+print(c, d)
