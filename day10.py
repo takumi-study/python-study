9016b0c610ee24b58b882c78b3a07d67a827d26e
Takumi <jobtaku.25@gmail.com>
2026-03-10 23:13:46 +0900

    Add Newton's method practice and error explosion from Chamter 4

---
+++ day10.py
@@ -0,0 +1,49 @@
+# Chapter 4
+
+# x の平方根を求める
+x = 2
+
+r_new = x
+
+r1 = r_new
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
+
+r1 = r_new
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
+
+r1 = r_new
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
+
+r1 = r_new
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
+
+# 誤字によるエラー体験
+r1 = rbew
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
+
+# 他の数値や0の平方根を求める
+x = 4
+r_new = x
+
+r1 = r_new
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
+
+x = 0
+r_new = x
+
+r1 = r_new
+r2 = x/r1
+r_new = (r1 + r2)/2
+print(r1, r_new, r2)
