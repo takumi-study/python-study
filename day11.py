5e95219cd46ab027a1399998fcc360497544d0f5
Takumi <jobtaku.25@gmail.com>
2026-03-11 23:15:25 +0900

    Add list basics and list creation examoles

---
+++ day11.py
@@ -0,0 +1,21 @@
+a = [5, 1, 3, 4]
+print(a)
+print(a[0])
+print(a[2])
+
+c = 5
+a = [c, 1, 3, 4]
+print(a)
+
+e = list()
+n = list(range(5))
+print(e)
+print(n)
+
+s = list("abcde")
+print(s)
+
+t = "a textbook of Python"
+tlist = t.split()
+print(tlist)
+
