e177ce333d36a0276bfd4d248e04c1259968ccee
Takumi <jobtaku.25@gmail.com>
2026-04-02 23:56:22 +0900

    Understand global variables and mutable list behavior

---
+++ day33.py
@@ -0,0 +1,26 @@
+a = 10
+b = 0
+
+def f():
+    global b
+    c = a*a
+    b = c
+
+f()
+print(b, a)
+
+#
+a = 0
+def f():
+    global a
+    a = a + 1
+
+def g(x):
+    x[0] = 0
+
+f()
+print(a)
+b = [1, 2, 3]
+g(b)
+print(b)
+
